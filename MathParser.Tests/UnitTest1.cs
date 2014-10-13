2014bf2eb08b03d7b9bbe616b7ac906b721866c2
cambraidwood <cam@effective-web.net>
2014-10-13 16:37:24 +0100

    1

---
+++ MathParser.Tests/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathParser.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_Acceptance_Criteria_1()
+        {
+
+            Assert.AreEqual(MathParser.Program.parse("3a2c4"), 20);
+
+        }
+
+        [TestMethod]
+        public void Test_Acceptance_Criteria_2()
+        {
+
+            Assert.AreEqual(MathParser.Program.parse("32a2d2"), 17);
+
+        }
+
+        [TestMethod]
+        public void Test_Acceptance_Criteria_3()
+        {
+
+            Assert.AreEqual(MathParser.Program.parse("500a10b66c32"), 14208);
+
+        }
+
+        [TestMethod]
+        public void Test_Acceptance_Criteria_4()
+        {
+
+            Assert.AreEqual(MathParser.Program.parse("3ae4c66fb32"), 235);
+
+        }
+
+        [TestMethod]
+        public void Test_Acceptance_Criteria_5()
+        {
+
+            Assert.AreEqual(MathParser.Program.parse("3c4d2aee2a4c41fc4f"), 990);
+
+        }
+    }
+}
---
+++ MathParser/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MathParser
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static decimal parse(string input)
+        {
+
+            decimal partialResult = -1;
+            string question = string.Empty;
+            string partialQuestion = string.Empty;
+            string manipulate = input;
+
+            SimpleExpressionEvaluator.ExpressionEvaluator EV = new SimpleExpressionEvaluator.ExpressionEvaluator();
+
+            string reg = @"^[\d]{1,4}[abcd]{1}[\d]{1,4}";
+            string reg2 = @"[\d]{1,4}[abcd]{1}[\d]{1,4}";
+
+            Match m = Regex.Match(manipulate, reg);
+
+            while (m.Success)
+            {
+
+                partialQuestion = m.Value.Replace("a", "+");
+                partialQuestion = partialQuestion.Replace('b', '-');
+                partialQuestion = partialQuestion.Replace('c', '*');
+                partialQuestion = partialQuestion.Replace('d', '/');
+
+                partialResult = EV.Evaluate(partialQuestion);
+
+                manipulate = manipulate.Replace(m.Value, partialResult.ToString());
+
+                m = Regex.Match(manipulate, reg2);
+
+            }
+
+            question = manipulate.Replace("a", "+");
+            question = question.Replace('b', '-');
+            question = question.Replace('c', '*');
+            question = question.Replace('d', '/');
+            question = question.Replace('e', '(');
+            question = question.Replace('f', ')');
+
+            return EV.Evaluate(question);
+
+        }
+    }
+}
